0a4196b39972854ee7950413cf1a74d23fa48ded
pbentes <pedrogomes2000.3@gmail.com>
2025-02-26 23:50:32 +0000

    Reorganized the project

---
+++ language_mapping.ts
@@ -0,0 +1,4 @@
+/*
+        This file will contain a way of mapping internal dstypes to an
+    output language such as Typescript, C# or Go.
+*/
--- main.ts
+++ main.ts
@@ -1,119 +1,13 @@
-import { Parser } from "npm:@dbml/core";
+// deno-lint-ignore no-unused-vars
 import Handlebars from "npm:handlebars";
-import { DSFieldType, DSField, DSTable } from "./model.ts";
+import { DSTable } from "./dstypes.ts";
+import { ParseDbml } from "./dbml.ts";
 
-const dbml: string = Deno.readTextFileSync("test1.dbml");
-
-const parser = new Parser();
-const database = parser.parse(dbml, 'dbml');
-
-const tables: DSTable[] = []
-database.schemas.forEach(schema => {
-    schema.tables.forEach(table => {
-        const dstable: DSTable = new DSTable();
-        dstable.name = table.name;
-
-        table.fields.forEach(field => {
-            const dsfield: DSField = new DSField();
-            
-            dsfield.name = field.name;
-            dsfield.type = MapType(field.type.type_name)
-            dsfield.type_args = field.type.args || "";
-            dsfield.not_null = !!field.not_null;
-            dsfield.pk = !!field.pk;
-            dsfield.unique = !!field.unique;
-            dsfield.increment = !!field.increment;
-
-            dstable.fields.push(dsfield);
-        });
-
-        tables.push(dstable);
-    });
-});
-
-const _template = Handlebars.compile("");
-
-function MapType(type: string): DSFieldType {
-    let retType: DSFieldType = DSFieldType.Unknown;
-
-    switch(type) {
-        case "tinyint":
-            retType = DSFieldType.TinyInt;
-            break;
-        case "smallint":
-            retType = DSFieldType.SmallInt;
-            break;
-        case "int":
-        case "integer":
-            retType = DSFieldType.Int;
-            break;
-        case "bigint":
-            retType = DSFieldType.BigInt;
-            break;
-
-        case "decimal":
-            retType = DSFieldType.Decimal;
-            break;
-        case "float":
-            retType = DSFieldType.Float;
-            break;
-        case "double":
-            retType = DSFieldType.Double;
-            break;
-        
-        case "char":
-        case "nchar":
-            retType = DSFieldType.Char;
-            break;
-        
-        case "varchar":
-        case "nvarchar":
-        case "text":
-        case "ntext":
-            retType = DSFieldType.VarChar;
-            break;
-        
-        case "date":
-            retType = DSFieldType.Date;
-            break;
-        case "time":
-            retType = DSFieldType.Time;
-            break;
-        case "datetime":
-            retType = DSFieldType.DateTime;
-            break;
-        case "timestamp":
-            retType = DSFieldType.TimeStamp;
-            break;
-
-        case "binary":
-        case "varbinary":
-        case "blob":
-        case "bytea":
-        case "image":
-            retType = DSFieldType.Binary;
-            break;
-        
-        case "bit":
-        case "boolean":
-        case "bool":
-            retType = DSFieldType.Bit;
-            break;
-        
-        case "enum":
-            retType = DSFieldType.Enum;
-            break;
-        case "set":
-            retType = DSFieldType.Set;
-            break;
-    
-        default:
-            if(type.includes("varchar")) {
-                retType = DSFieldType.VarChar;
-            }
-    }
-
-    return retType;
+function main() {
+    const tables: DSTable[] = ParseDbml("test1.dbml");
+    console.log(tables)
 }
 
-console.log(tables)
+if (import.meta.main) {
+    main();
+}
